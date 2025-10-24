a30ab9538258ad3f8d163f72122019f4aaa4b608
Ayush <85547808+AyushK-26@users.noreply.github.com>
2025-10-25 01:25:18 +0530

    leftRotateByOnePlace

---
+++ Arrays/ArraysEasy/leftRotateByOnePlace.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// OPTIMAL      TC = O(n)       SC = o(1)
+void optimal(vector<int> &arr, int n) {
+    int temp = arr[0];
+    for(int i = 1; i < n; i++) {
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+}
+
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+
+    for(int &i : arr) cin>>i;
+
+    optimal(arr, n);
+
+    for(int n : arr) {
+        cout<<n<<" ";
+    }
+
+}
