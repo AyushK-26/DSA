f421b13ef9d57046b284b7e87323370cbdc41013
Ayush <85547808+AyushK-26@users.noreply.github.com>
2025-10-27 01:55:47 +0530

    linearSearch

---
+++ Arrays/ArraysEasy/linearSearch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+// OPTIMAL      TC = O(n)       SC = O(1)
+int optimal(vector<int> arr, int n, int target) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == target) return i;
+    }
+    return -1;
+}
+
+int main() {
+    int n, target;
+    cin>>n>>target;
+    
+    vector<int> arr(n);
+    for(int &i : arr) cin>>i;
+
+    cout<<optimal(arr, n, target);
+}
