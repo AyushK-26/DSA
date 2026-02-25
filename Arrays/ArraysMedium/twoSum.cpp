db82f9dbcef1202d4f1dc5bcb53a06ea120aa4a0
Ayush <85547808+AyushK-26@users.noreply.github.com>
2026-02-25 23:05:47 +0530

    twoSum

--- Arrays/ArraysMedium/twoSum.cpp
+++ Arrays/ArraysMedium/twoSum.cpp
@@ -1,33 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <algorithm>
+#include <unordered_set>
 using namespace std;
 
-//  Returning indecies of the elements.
-
 // BRUTE        ->      TC = O(n^2)     SC = O(1)
-vector<int> brute(vector<int> arr, int n, int k) {
+bool brute(vector<int> arr, int n, int k) {
     for(int i = 0; i < n; i++) {
         for(int j = i+1; j < n; j++) {
-            if(arr[i] + arr[j] == k) return {i, j};
+            if(arr[i] + arr[j] == k) return true;
         }
     }
-    return {-1, -1};
+    return false;
 }
 
 
-// BETTER        ->      TC = O(n): unordered_map (avg case)   -   O(nlogn): map     SC = O(n)
-vector<int> optimal(vector<int> arr, int n, int k) {
-    unordered_map<int, int> hashmap;
-    hashmap[arr[0]] = 0;
-
-    for(int i = 0; i < n; i++) {
-        if(hashmap.find(k-arr[i]) != hashmap.end()) {
-            return {hashmap[k-arr[i]], i};
-        }
-        hashmap[arr[i]] = i;
+// BETTER       ->      TC = O(nlogn + n)     SC = O(1)
+bool better(vector<int> arr, int n, int k) {
+    sort(arr.begin(), arr.end());
+    
+    int i = 0, j = n-1;
+    while(i < j) {
+        if(arr[i] + arr[j] < k) i++;
+        else if(arr[i] + arr[j] > k) j--;
+        else return true;
     }
-    return {-1, -1};
+
+    return false;
+}
+
+
+// OPTIMAL       ->      TC = O(n)     SC = O(n)
+bool optimal(vector<int> arr, int n, int k) {
+    unordered_set<int> presentElements;
+    
+    for(int i = 0; i < n; i++) {
+        if(presentElements.find(k-arr[i]) != presentElements.end()) {
+            return true;
+        }
+        presentElements.insert(arr[i]);
+    }
+    
+    return false;
 }
 
 int main() {
@@ -37,13 +51,9 @@ int main() {
     vector<int> arr(n);
     for(int &i : arr) cin>>i;
 
-    vector<int> bruteAns = brute(arr, n, k);
-    for(int x : bruteAns) cout<<x<<" ";
-
-    cout<<endl;
-    
-    vector<int> optimalAns = optimal(arr, n, k);
-    for(int x : optimalAns) cout<<x<<" ";
+    cout<<brute(arr, n, k)<<endl;
+    cout<<better(arr, n, k)<<endl;
+    cout<<optimal(arr, n, k)<<endl;
 
     return 0;
 }
