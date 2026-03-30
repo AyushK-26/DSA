e838a041c59c6a56719223597587ddc6d925bfab
Ayush <85547808+AyushK-26@users.noreply.github.com>
2026-03-30 18:17:50 +0530

    fix: optimize optimal function to avoid duplicate quadruplets

--- Arrays/ArraysHard/fourSum.cpp
+++ Arrays/ArraysHard/fourSum.cpp
@@ -68,7 +68,10 @@ vector<vector<int>> optimal(vector<int> arr, int n, int target) {
     sort(arr.begin(), arr.end());
 
     for(int i = 0; i < n; i++) {
+        if(i != 0 && arr[i] == arr[i-1]) continue;
         for(int j = i+1; j < n; j++) {
+            if(j != i+1 && arr[j] == arr[j-1]) continue;
+
             int k = j+1;
             int l = n-1;
 
