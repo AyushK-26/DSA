1527768b85243ba0a05df8582f3fb75ad3665929
Ayush <85547808+AyushK-26@users.noreply.github.com>
2026-05-22 13:38:33 +0530

    searchInRotatedSortedArray

---
+++ Binary Search/BS on 1D Arrays/searchInRotatedSortedArray.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+// OPTIMAL      ->      TC = O(log*n)     SC = O(1)
+int optimal(vector<int> arr, int n, int target) {
+
+    int low = 0;
+    int high = n-1;
+
+    while(low <= high) {
+        int mid = low+(high-low)/2;
+
+        if(arr[mid] == target) return mid;
+
+        if(arr[low] <= arr[mid]) {
+            // low -> mid: sorted part
+
+            if(arr[low] <= target && arr[mid] > target) high = mid-1;
+            else low = mid+1;
+        }else {
+            // mid -> high: sorted part
+
+            if(arr[mid] < target && arr[high] >= target) low = low+1;
+            else high = mid-1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int n, target;
+    cin>>n>>target;
+
+    vector<int> arr(n);
+    
+    for(int &i : arr) cin>>i;
+
+    cout<<optimal(arr, n, target)<<endl;
+
+    return 0;
+}
