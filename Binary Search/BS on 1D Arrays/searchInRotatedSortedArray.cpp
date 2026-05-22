1ca98f53186ff39d9e871c577b7a8f7f4f5899c3
Ayush <85547808+AyushK-26@users.noreply.github.com>
2026-05-22 15:56:33 +0530

    searchInRotatedSortedArrayDups

--- Binary Search/BS on 1D Arrays/searchInRotatedSortedArray.cpp
+++ Binary Search/BS on 1D Arrays/searchInRotatedSortedArray.cpp
@@ -17,12 +17,12 @@ int optimal(vector<int> arr, int n, int target) {
         if(arr[low] <= arr[mid]) {
             // low -> mid: sorted part
 
-            if(arr[low] <= target && arr[mid] > target) high = mid-1;
+            if(arr[low] <= target && target < arr[mid] ) high = mid-1;
             else low = mid+1;
         }else {
             // mid -> high: sorted part
 
-            if(arr[mid] < target && arr[high] >= target) low = low+1;
+            if(arr[mid] < target && target <= arr[high] ) low = low+1;
             else high = mid-1;
         }
     }
---
+++ Binary Search/BS on 1D Arrays/searchInRotatedSortedArrayDups.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// OPTIMAL      ->      TC = O(n/2): Worst Case    SC = O(1)
+//                      TC = Θ(log*n): Avg. Case 
+bool optimal(vector<int> arr, int n, int target) {
+    int low = 0;
+    int high = n-1;
+
+    while(low <= high) {
+        int mid = low+(high-low)/2;
+
+        if(arr[mid] == target) return true;
+        if(arr[low] == arr[mid] && arr[mid] == arr[high]) {
+            low++;
+            high--;
+            continue;
+        }
+
+        if(arr[low] <= arr[mid]) {
+            // low -> mid: sorted part
+            if(arr[low] <= target && target < arr[mid]) high = mid-1;
+            else low = mid+1;
+        } else {
+            // mid -> high: sorted part
+            if(arr[mid] < target && target <= arr[high]) low = low+1;
+            else high = high-1;
+        }
+    }
+
+    return false;
+
+}
+
+int main () {
+    int n, target;
+    cin>>n>>target;
+
+    vector<int> arr(n);
+    for(int &i : arr) cin>>i;
+
+    cout<<optimal(arr, n, target)<<endl;
+}
