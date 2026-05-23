76420647bbdd399d570a2ba14d457c89f8718914
Ayush <85547808+AyushK-26@users.noreply.github.com>
2026-05-23 08:33:07 +0530

    singleElementInSortedArray

---
+++ Binary Search/BS on 1D Arrays/singleElementInSortedArray.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int optimal(vector<int> arr, int n) {
+    int low = 1;
+    int high = n-2;
+
+    if(n == 1) return arr[0];
+    if(arr[0] != arr[1]) return arr[0];
+    if(arr[n-1] != arr[n-2]) return arr[n-1];
+
+    while(low <= high) {
+        int mid = low+(high-low)/2;
+
+        if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) {
+            return arr[mid];
+        }
+
+        if(arr[mid] == arr[mid-1]) {
+            if(mid % 2 == 0) high = mid-2;
+            else low = mid+1;
+        } else {
+            if(mid % 2 == 0) low = mid+2; 
+            else high = mid-1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+    for(int &i : arr) cin>>i;
+
+    cout<<optimal(arr, n)<<endl;
+
+    return 0;
+}
