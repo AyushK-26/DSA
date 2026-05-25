3212813b0b04a39b390bfc1dfb9ac3680f0ab18b
Ayush <85547808+AyushK-26@users.noreply.github.com>
2026-05-25 20:00:20 +0530

    aggressiveCows

---
+++ Binary Search/BS on Answers/aggressiveCows.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+// OPTIMAL      ->      TC = O(n * logn) + O(n * log(maxCoordinate − minCoordinate))    SC = O(1)
+bool isPossible(vector<int> coordinates, int n, int cows, int distance) {
+    int cowsPlaced = 1;
+    int lastCowPlaced = coordinates[0];
+
+    for(int i = 1; i < n; i++) {
+        if(coordinates[i]-lastCowPlaced >= distance) {
+            cowsPlaced++;
+            lastCowPlaced = coordinates[i];
+        }
+    }
+
+    return cowsPlaced >= cows; 
+}
+
+int optimal(vector<int> coordinates, int n, int cows) {
+
+    sort(coordinates.begin(), coordinates.end());
+
+    int low = 1;
+    int high = coordinates[n-1]-coordinates[0];
+
+    while(low <= high) {
+        int mid = low+(high-low)/2;
+
+        if(isPossible(coordinates, n, cows, mid)) low = mid+1;
+        else high = mid-1;
+    }
+
+    return high;
+}
+
+
+int main () {
+    int n, cows;
+    cin>>n>>cows;
+
+    vector<int> coordinates(n);
+    for(int &i : coordinates) cin>>i;
+
+    cout<<optimal(coordinates, n, cows)<<endl;
+
+    return 0;
+}
