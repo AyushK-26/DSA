7808118df61811ea8bbd9536858285c516424dd7
Ayush <85547808+AyushK-26@users.noreply.github.com>
2026-06-19 19:04:24 +0530

    sortLLWithZeroesOnesTwos

---
+++ Linked List/LinkedListMedium/sortLLWithZeroesOnesTwos.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node() {
+        this->data = 0;
+        this->next = nullptr;
+    }
+
+    Node(int data, Node* next = nullptr) {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node* convertToLL(vector<int> arr) {
+    if(arr.size() == 0) return nullptr;
+
+    Node* head = new Node(arr[0]);
+    Node* temp = head;
+    for(int i = 1; i < arr.size(); i++) {
+        Node* nodeToInsert = new Node(arr[i]);
+        temp->next = nodeToInsert;
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+void traverseLL(Node* &head) {
+    Node* temp = head;
+    while(temp) {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+Node* optimal(Node* head) {
+    Node fakeHead0, fakeHead1, fakeHead2;
+
+    Node* zeroMover = &fakeHead0;
+    Node* oneMover = &fakeHead1;
+    Node* twoMover = &fakeHead2;
+
+    Node* temp = head;
+    while(temp) {
+        if(temp->data == 0) {
+            zeroMover->next = temp;
+            zeroMover = zeroMover->next;
+        } else if(temp->data == 1) {
+            oneMover->next = temp;
+            oneMover = oneMover->next;
+        }else {
+            twoMover->next = temp;
+            twoMover = twoMover->next;
+        }
+
+        temp = temp->next;
+    }
+
+    if(fakeHead1.next) {
+        zeroMover->next = fakeHead1.next;
+        oneMover->next = fakeHead2.next;
+    } else {
+        zeroMover->next = fakeHead2.next;
+    }
+    twoMover->next = nullptr;
+
+    return fakeHead0.next;
+}
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+    for(int &i : arr) cin>>i;
+
+    Node* head = convertToLL(arr);
+    
+    head = optimal(head);
+    traverseLL(head);
+    
+    return 0;
+}
